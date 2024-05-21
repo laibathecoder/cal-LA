091d9342d80edfe09c6e8a277951bad11271a4d1
laibathecoder <167254061+laibathecoder@users.noreply.github.com>
2024-05-22 03:04:25 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+//Command Line Calculator:
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {message: "Enter your First number", type: "number", name: "firstNumber" },
+  {message: "Enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one Operator to perfome a Opration",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Multiplication", "Subtraction", "Division"]
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Please select correct operator");
+}
